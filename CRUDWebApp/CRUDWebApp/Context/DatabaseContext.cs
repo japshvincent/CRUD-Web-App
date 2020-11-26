b507a356900cf0d0bad292f78995005a7f1f6dc8
Stan Bubbers <stan.bubbers@asos.com>
2020-11-26 11:54:51 +0000

    Add user interface

--- CRUDWebApp/CRUDWebApp/Context/DatabaseContext.cs
+++ CRUDWebApp/CRUDWebApp/Context/DatabaseContext.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using CRUDWebApp.Models;
+﻿using CRUDWebApp.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace CRUDWebApp.Context
---
+++ CRUDWebApp/CRUDWebApp/IService/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using CRUDWebApp.Models;
+
+namespace CRUDWebApp.IService
+{
+    public interface IUserService
+    {
+        List<User> GetUsers();
+        User GetUserById(int userId);
+        void Save(User user);
+        void Update(User user);
+        string Delete(int userId);
+    }
+}
