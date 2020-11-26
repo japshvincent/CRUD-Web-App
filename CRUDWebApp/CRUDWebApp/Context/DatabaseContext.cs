9872cdab8a3c5ed296437ca23a75c1b9cc86a689
Stan Bubbers <stan.bubbers@asos.com>
2020-11-26 11:45:10 +0000

    Add user model and database context

---
+++ CRUDWebApp/CRUDWebApp/Context/DatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CRUDWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDWebApp.Context
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ CRUDWebApp/CRUDWebApp/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CRUDWebApp.Models
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; } = 0;
+        public string Name { get; set; } = "";
+        public string Department { get; set; } = "";
+    }
+}
